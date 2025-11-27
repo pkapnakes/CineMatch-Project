a64a0c3a69f319587a35c424b6ef5f14af269b7b
cHasanDede <xasanneapoli@gmail.com>
2025-11-27 17:31:49 +0200

    h vasi litourgei kai epikinonei teleia

--- CineMatch-Project/src/main/java/com/cinematch/project/controllers/ViewController.java
+++ CineMatch-Project/src/main/java/com/cinematch/project/controllers/ViewController.java
@@ -1,21 +1,22 @@
 package com.cinematch.project.controllers;
 
+import com.cinematch.project.models.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class ViewController {
 
-    @GetMapping("/login")
-    public String loginPage(@RequestParam(required = false) String error, Model model) {
-        model.addAttribute("error", error);
-        return "login";
-    }
+
 
     @GetMapping("/home")
     public String homePage(@RequestParam(required = false) String username, Model model) {
+
+
         model.addAttribute("username", username);
         return "home";
     }
